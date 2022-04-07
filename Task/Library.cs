d764af47da19428679d7bf3e84c4d55ce8292140
SeymurMs <99805897+SeymurMs@users.noreply.github.com>
2022-04-07 14:09:34 +0400

    Add files via upload

---
+++ Task/Library.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7AprelTask
+{
+    internal class Library
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Book> books { get; set; } = new List<Book>();
+        
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+        public Book GetBookById(int? id)
+        {
+            if (id == null)
+                throw new ArgumentNullException("Xeta!");
+
+           Book book = books.Find(x => x.Id == id);
+            if (book == null)
+                throw new ArgumentNullException("Xeta!");
+            return book;            
+        }
+        public  void RemoveBook(int? id)
+        {
+            if (id == null)
+                throw new ArgumentNullException("Xeta!");
+            Book book = books.Find(x => x.Id == id);
+            if (book == null)
+                throw new ArgumentNullException("Xeta!");
+            books.Remove(book);
+        }
+    }
+}
